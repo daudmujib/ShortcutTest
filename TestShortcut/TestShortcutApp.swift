497e8ca9d6f6afe5634923ad3bead5081656d441
Daud Mujib <dev.daudmujib@gmail.com>
2025-06-11 16:31:22 +0500

    Initial Commit

---
+++ TestShortcut/TestShortcutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestShortcutApp.swift
+//  TestShortcut
+//
+//  Created by Daud Mujib on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestShortcutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
